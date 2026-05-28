2001e680995a64c68c2a4f32c6dc1d8f322cc77d
abhinavvgoell <abhinavgoel206@gmail.com>
2026-05-28 19:18:55 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.java
@@ -0,0 +1,35 @@
+//IT SEEMS EASY BUT IT ISNT THE DECODE PART IS TOUGH BUT NOT THAT TOUGH
+class Solution {
+    public String encode(List<String> strs) {
+        String encoded_string = "";
+        int n = strs.size();
+        for (int i = 0; i < n; i++) {
+            String temp = strs.get(i);
+            int l = temp.length();
+            temp = l + "#" + temp;
+            encoded_string = encoded_string + temp;
+        }
+        return encoded_string;
+    }
+    public List<String> decode(String str) {
+        ArrayList<String> list = new ArrayList<>();
+        int n = str.length();
+        int i = 0;
+        int m = 0;
+
+        while (i < n) {
+            m = str.charAt(i) - '0';
+            int a = 1;
+            while (str.charAt(i + a) != '#') {
+                int k = str.charAt(i + a) - '0';
+                m = m * 10 + k;
+                a++;
+            }
+            i = i + 2 + a - 1;
+            String temp = str.substring(i, i + m);
+            i = i + m;
+            list.add(temp);
+        }
+        return list;
+    }
+}
